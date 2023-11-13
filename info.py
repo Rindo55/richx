6faccaebf1fdc26f8b1991009b97120d737608a0
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-11-14 00:04:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ SESSION = 'Media_search'
 API_ID = 10247139
 API_HASH = "96b46175824223a33737657ab943fd6a"
 BOT_TOKEN = "5786017840:AAEsbeA-1QUdr_0Stp3Bg8V0ov0kxl1A_28"
-PORT = "8081"
+PORT = "8084"
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
